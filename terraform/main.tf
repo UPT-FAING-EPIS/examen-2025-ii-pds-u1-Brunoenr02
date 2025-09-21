4a632490115a19fffa9f416ffdd072d9f9501469
Brunoenr02 <brunoancco02@gmail.com>
2025-09-21 03:04:53 -0500

    desplieguev6

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,7 +86,7 @@ resource "google_artifact_registry_repository" "babysitter_repo" {
   description   = "Docker repository for Babysitter application"
   format        = "DOCKER"
   depends_on    = [google_project_service.services]
-  
+
   # Handle existing repository
   lifecycle {
     ignore_changes = [
